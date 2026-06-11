6c77380e98b1f3c0ea2b75889c40eda11c2ce501
SYEDRIYANSOC <syedriyan2435@gmail.com>
2026-06-11 20:37:58 +0530

    Update universal_shift_register.v

--- 03_Sequential_Design/universal_shift_register.v
+++ 03_Sequential_Design/universal_shift_register.v
@@ -1,34 +1,36 @@
-module universal_shift_register (
+module universal_shift_register #(
+    parameter WIDTH = 4
+)(
     input clk,
     input rst,
     input [1:0] sel,
     input serial_left,
     input serial_right,
-    input [3:0] parallel_in,
-    output reg [3:0] q
+    input [WIDTH-1:0] parallel_in,
+    output reg [WIDTH-1:0] q
 );
 
 always @(posedge clk) begin
     if (rst) begin
-        q <= 4'b0000;
+        q <= {WIDTH{1'b0}};
     end
     else begin
         case (sel)
 
             2'b00: begin
-                q <= q;
+                q <= q;  // hold
             end
 
             2'b01: begin
-                q <= {serial_right, q[3:1]};
+                q <= {serial_right, q[WIDTH-1:1]};  // shift right
             end
 
             2'b10: begin
-                q <= {q[2:0], serial_left};
+                q <= {q[WIDTH-2:0], serial_left};   // shift left
             end
 
             2'b11: begin
-                q <= parallel_in;
+                q <= parallel_in;  // parallel load
             end
 
             default: begin
