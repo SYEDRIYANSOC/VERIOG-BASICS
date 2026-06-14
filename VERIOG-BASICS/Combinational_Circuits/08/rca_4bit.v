679b34a1fd5b0dcb1b49d531dbca65c6509bfc01
SYEDRIYANSOC <syedriyan2435@gmail.com>
2026-06-14 15:04:50 +0530

    Create rca_4bit.v

---
+++ VERIOG-BASICS/Combinational_Circuits/08/rca_4bit.v
@@ -0,0 +1,23 @@
+// Full Adder
+module full_adder (
+    input  wire a, b, cin,
+    output wire sum, cout
+);
+    assign sum  = a ^ b ^ cin;
+    assign cout = (a & b) | (cin & (a ^ b));
+endmodule
+
+// 4-bit Ripple Carry Adder
+module rca_4bit (
+    input  wire [3:0] a, b,
+    input  wire       cin,
+    output wire [3:0] sum,
+    output wire       cout
+);
+    wire c1, c2, c3;   // intermediate carries
+
+    full_adder FA0 (.a(a[0]), .b(b[0]), .cin(cin), .sum(sum[0]), .cout(c1));
+    full_adder FA1 (.a(a[1]), .b(b[1]), .cin(c1),  .sum(sum[1]), .cout(c2));
+    full_adder FA2 (.a(a[2]), .b(b[2]), .cin(c2),  .sum(sum[2]), .cout(c3));
+    full_adder FA3 (.a(a[3]), .b(b[3]), .cin(c3),  .sum(sum[3]), .cout(cout));
+endmodule
